5fefab990616bbe2d2d1bd269c89524320915ab7
dilanMD <deluxan.sgic@gmail.com>
2020-07-01 15:03:08 +0530

    Sidebar collapse and open

--- src/layouts/Sidebar.js
+++ src/layouts/Sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { MdMail } from "react-icons/md";
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 
 import Card from "../components/Card/Card";
 import { COLORS } from "../constants/colors";
@@ -8,8 +9,34 @@ import Menu from "./Menu";
 
 const Sidebar = () => {
   const { green, white } = COLORS;
+
+  const collapseHandler = () => {
+    let sidebar = document.getElementById("sidebar");
+    sidebar.style.left = "-180px";
+    setTimeout(() => {
+      let openIcon = document.getElementById("open-icon");
+      openIcon.style.visibility = "visible";
+    }, 1000);
+  };
+
+  const openHandler = () => {
+    let sidebar = document.getElementById("sidebar");
+    sidebar.style.left = "0px";
+    setTimeout(() => {
+      let openIcon = document.getElementById("open-icon");
+      openIcon.style.visibility = "hidden";
+    }, 1000);
+  };
+
   return (
-    <div className="sidebar" style={{ backgroundColor: green }}>
+    <div id="sidebar" style={{ backgroundColor: green }}>
+      {/* Open icon */}
+      <FaArrowAltCircleRight
+        id="open-icon"
+        size={30}
+        color={green}
+        onClick={() => openHandler()}
+      />
       <div className="sidebar-contents">
         <img src={ProfilePic} alt="Profile Pic" className="profile-pic" />
         <h5 className="profile-name" style={{ color: white }}>
@@ -19,9 +46,19 @@ const Sidebar = () => {
           UI/UX Engineer
         </h5>
         <Menu />
+        {/* Message icon with number of unread messages */}
         <div className="message-icon-container">
           <MdMail className="message-icon" size={30} color={green} />
           <span style={{ backgroundColor: green, color: white }}>2</span>
+        </div>
+        {/* Collapse icon */}
+        <div className="collapse-container">
+          <FaArrowAltCircleLeft
+            className="collapse-icon"
+            size={30}
+            color={white}
+            onClick={() => collapseHandler()}
+          />
         </div>
       </div>
     </div>
